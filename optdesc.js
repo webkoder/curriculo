0df35349c1bf325745e2f88f4fa49361d5bef21c
Ricardo <ricardofrr@gmail.com>
2017-09-08 23:10:30 -0300

    optdesc

--- optdesc.js
+++ optdesc.js
@@ -1,9 +1,16 @@
-var btnsmostrar = document.getElementsByClassName('mostrardescricao');
+var _optdesc = document.getElementsByClassName('optdesc');
 
-for (var i = 0; i < btnsmostrar.length; i++) {
-    var btn = btnsmostrar[i];
+for (var c = 0; c < _optdesc.length; c++) {
+    var bloco = _optdesc[c];
+    console.log(bloco.children);
+    var d = document.createElement('div');
+    d.className = 'mostrardescricao';
+    d.dataset.secao = bloco.dataset.secao;
+    d.innerHTML = '<div><span class="mais"></span><i class="fa fa-angle-down fa-2x imais" aria-hidden="true"></i></div>';
 
-    btn.addEventListener('click', function(e){
+    bloco.children[0].id = bloco.dataset.secao;
+    
+    d.addEventListener('click', function(e){
         var secao = this.dataset.secao;
         if(this.children[0].children[0].className == 'menos'){
             document.getElementById(secao).className= '';
@@ -14,7 +21,8 @@ for (var i = 0; i < btnsmostrar.length; i++) {
             this.children[0].children[0].className = 'menos';
             this.children[0].children[1].className = 'fa fa-angle-down fa-2x imenos';
         }
-
+        
     });
-    
+
+    bloco.appendChild(d);
 }
